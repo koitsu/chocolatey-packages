76105a6fb33b785a633d249afe7153be92a9b2ca
ComFreek <comfreek@outlook.com>
2017-11-05 11:26:24 +0100

    AU: 1 updated - hxd [skip ci] https://gist.github.com/e79979923e431062cc787da50364eef1/44378d0f35ddc651549c8c26d179e3e3c1bfd6b4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,66 +1,20 @@
-$ErrorActionPreference = 'Stop';
-
-if (-not(Get-Command ConvertFrom-Json -ErrorAction SilentlyContinue)) {
-
-	# Following ConvertFrom-Json polyfill is based on http://stackoverflow.com/a/29689642
-	# Thanks to its author Edward (http://stackoverflow.com/users/2934838/edward)
-	# License is CC BY-SA 3.0
-
-	# This requires .NET 3.5, we therefore depend on Chocolatey 0.9.10.x in the nuspec file
-	Add-Type -Assembly System.Web.Extensions
-	function ConvertFrom-Json {
-		param(
-			[Parameter(ValueFromPipeline = $true)] $json
-		)
-
-		$ps_js = New-Object System.Web.Script.Serialization.JavaScriptSerializer
-
-		# The comma operator is the array construction operator in PowerShell
-		return ,$ps_js.DeserializeObject($json)
-	}
-}
-
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'HxD'
+$url32       = 'https://mh-nexus.de/downloads/HxDSetup.zip'
+$checksum32  = ''
+
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-. "$toolsDir\languages.ps1"
-
-$zipLocation = Join-Path $toolsDir 'setup.zip'
-$setupLocation = Join-Path $toolsDir 'setup.exe'
-$hashLocation = Join-Path $toolsDir 'hashes.json'
-
-$fallbackLanguage = 'en'
-
-$packageParameters = $env:chocolateyPackageParameters
-if ($packageParameters) {
-	$argumentMap = ConvertFrom-StringData $packageParameters
-	$passedLanguage = $argumentMap.Item('lang')
-}
-
-$installLanguage = $fallbackLanguage
-$availableLanguages = Get-AvailableLanguages
-
-$systemLanguage = (Get-Culture).TwoLetterISOLanguageName.toLower()
-if ($availableLanguages.ContainsKey($systemLanguage)) {
-	$installLanguage = $systemLanguage
-}
-
-if ($passedLanguage -and $availableLanguages.ContainsKey($passedLanguage)) {
-	$installLanguage = $passedLanguage
-}
-
-$url = "https://mh-nexus.de/downloads/HxDSetup$($availableLanguages.Get_Item($installLanguage)).zip" 
-
-$checksum = ((Get-Content $hashLocation | Out-String | ConvertFrom-Json) | Where-Object { $_.lang -eq $installLanguage }).hash
-$checksumType = 'sha256'
+$zipLocation = Join-Path $toolsDir 'HxDSetup.zip'
+$setupLocation = Join-Path $toolsDir 'HxDSetup.exe'
 
 $webArgs = @{
 	packageName  = $packageName
 	fileFullPath = $zipLocation
-	url          = $url
-	checksum     = $checksum
-	checksumType = $checksumType
+	url          = $url32
+	checksum     = $checksum32
+	checksumType = 'sha256'
 }
 
 Get-ChocolateyWebFile @webArgs
