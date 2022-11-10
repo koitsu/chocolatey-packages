b680b1358fb870acc6095d3a6629c75d23f57056
Jeremy Chadwick <jdc@koitsu.org>
2022-11-09 21:29:55 -0800

    asm6f: solve x86/x64 binary confusion for ShimGen

--- asm6f/tools/chocolateyInstall.ps1
+++ asm6f/tools/chocolateyInstall.ps1
@@ -1,16 +1,20 @@
-﻿# Based on two sources:
+﻿# Based on sources:
 # https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/sysinternals/tools/chocolateyInstall.ps1
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
 # https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $pp = Get-PackageParameters
 $installDir = $toolsPath
 
-if ($pp.InstallDir -or $pp.InstallationPath ) { $InstallDir = $pp.InstallDir + $pp.InstallationPath }
+if ($pp.InstallDir -or $pp.InstallationPath) {
+  $InstallDir = $pp.InstallDir + $pp.InstallationPath
+}
+
 Write-Host "asm6f is going to be installed in '$installDir'"
 
 $packageArgs = @{
-  packageName     = 'asm6f'
+  packageName     = $env:chocolateyPackageName
   url             = 'https://github.com/freem/asm6f/releases/download/v1.6_freem02/asm6f_20181019.zip'
   checksum        = 'd5ab4445f4624e801748ac2b3cab4d6560721a1c302456c44d53d1e353c92ef3'
   checksumType    = 'sha256'
@@ -19,12 +23,12 @@ $packageArgs = @{
 
 Install-ChocolateyZipPackage @packageArgs
 
-# $zipFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
-#          Write-Host "Getting x64 bit zip"; Get-Item "$toolsDir\*_x64.zip"
-# } else { Write-Host "Getting x32 bit zip"; Get-Item "$toolsDir\*_x32.zip" }
-#
-# Get-ChocolateyUnzip -FileFullPath $zipfile -Destination $toolsDir
-#
-# # don't need zips anymore
-# Remove-Item ($toolsDir + '\*.' + 'zip')
-
+if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
+  Write-Host "x64 architecture detected, using asm6f_64.exe exclusively"
+  Remove-Item "$installDir\asm6f_32.exe"
+  Rename-Item "$installDir\asm6f_64.exe" "asm6f.exe"
+} else {
+  Write-Host "x86 architecture detected, using asm6f_32.exe exclusively"
+  Remove-Item "$installDir\asm6f_64.exe"
+  Rename-Item "$installDir\asm6f_32.exe" "asm6f.exe"
+}
