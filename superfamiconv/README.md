6ea0f947417bb7eb1ee23b126988a632362b1e37
Jeremy Chadwick <jdc@koitsu.org>
2022-11-10 22:44:05 -0800

    superfamiconv: initial commit

---
+++ superfamiconv/README.md
@@ -0,0 +1,7 @@
+# SuperFamiconv
+
+This is the source repository for the Chocolately package named
+[SuperFamiconv](https://community.chocolatey.org/packages/superfamiconv).
+
+For details about the SuperFamiconv utility itself, please refer to
+[SuperFamiconv](https://github.com/Optiroc/SuperFamiconv).
---
+++ superfamiconv/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿# Based on sources:
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/sysinternals/tools/chocolateyInstall.ps1
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$pp = Get-PackageParameters
+$installDir = $toolsPath
+
+if ($pp.InstallDir -or $pp.InstallationPath) {
+  $InstallDir = $pp.InstallDir + $pp.InstallationPath
+}
+
+Write-Host "SuperFamiconv is going to be installed in: $installDir"
+
+$packageArgs = @{
+  packageName     = $env:chocolateyPackageName
+  url             = 'https://github.com/Optiroc/SuperFamiconv/releases/download/v0.9.2/SuperFamiconv-v0.9.2-win_x64.zip'
+  checksum64      = 'd8e3ebcacda5092dd39a54d73ebbccac1e590cabef2cdc4bd0721cf1b58908c7'
+  checksumType64  = 'sha256'
+  unzipLocation   = $installDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
