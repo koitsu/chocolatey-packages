587d3161800ee9a5965e48d5487dc406af0ee41b
Jeremy Chadwick <jdc@koitsu.org>
2022-11-11 11:34:11 -0800

    Use url64bit instead, stop install on errors via ErrorActionPreference

--- superfamiconv/tools/chocolateyInstall.ps1
+++ superfamiconv/tools/chocolateyInstall.ps1
@@ -3,9 +3,7 @@
 # https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
 # https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
 
-if ((Get-OSArchitectureWidth 32) -or $env:chocolateyForceX86 -eq 'true') {
-  throw 'x86 architecture detected. SuperFamiconv binaries are only available for 64-bit systems.'
-}
+$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $pp = Get-PackageParameters
@@ -19,7 +17,7 @@ Write-Host "SuperFamiconv is going to be installed in: $installDir"
 
 $packageArgs = @{
   packageName     = $env:chocolateyPackageName
-  url             = 'https://github.com/Optiroc/SuperFamiconv/releases/download/v0.9.2/SuperFamiconv-v0.9.2-win_x64.zip'
+  url64bit        = 'https://github.com/Optiroc/SuperFamiconv/releases/download/v0.9.2/SuperFamiconv-v0.9.2-win_x64.zip'
   checksum64      = 'd8e3ebcacda5092dd39a54d73ebbccac1e590cabef2cdc4bd0721cf1b58908c7'
   checksumType64  = 'sha256'
   unzipLocation   = $installDir
