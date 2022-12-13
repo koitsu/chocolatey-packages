8e92eab1812d700a683050f1c40ae9faecbd7c62
Jeremy Chadwick <jdc@koitsu.org>
2022-12-13 01:46:04 -0800

    hxd: add checksum validation to updater script

--- hxd/update.sh
+++ hxd/update.sh
@@ -6,11 +6,13 @@ t1=$(mktemp)
 curl -s -S -o "${t1}" 'https://mh-nexus.de/updates/HxDCurrentVersion.json'
 version=$(cat "${t1}" | jq -r '.Version')
 zipurl=$(cat "${t1}" | jq -r '."Download-URI"')
+jsonchecksum=$(cat "${t1}" | jq -r '."Unique-File-IDs"."SHA-512"' | tr 'A-F' 'a-f')
 
 # Version 2.5.0.0 testing
 # curl -s -S -o "${t1}" 'https://mh-nexus.de/HxD2.5.0.0-installable-BCP47-Language-Tags.json'
 # version=$(cat "${t1}" | jq -r '.Version')
 # zipurl=$(cat "${t1}" | jq -r '."Download-URI"."*"')
+# jsonchecksum=$(cat "${t1}" | jq -r '."Unique-File-IDs"."HxDSetup.zip"."SHA-512"' | tr 'A-F' 'a-f')
 
 # XXX debug
 # cat ${t1} | jq .
@@ -28,6 +30,22 @@ case "$(uname -s)" in
     ;;
   *) echo "ERROR: Unknown platform for sha512 checksum utility" ; exit 1
 esac
+
+# Ensure the checksum specified by the HxD author (in the JSON manifest)
+# matches what we got when downloading the .zip file ourselves.
+# If these don't match, then bail out with an error and DO NOT update
+# the Chocolatey package.
+# Ideally this should never happen, barring corrupt network traffic, MITM
+# attacks, local filesystem corruption, or (possibly) HxD maintainer
+# forgetting to update the checksum in the JSON but updating the .zip
+# file itself.
+
+if [[ "${checksum}" != "${jsonchecksum}" ]]; then
+  echo "ERROR: JSON manifest checksum does not match local .zip checksum!"
+  echo "JSON manifest: $jsonchecksum"
+  echo "Local:         $checksum"
+  exit 1
+fi
 
 echo "Latest release details:"
 echo "version:  $version"
