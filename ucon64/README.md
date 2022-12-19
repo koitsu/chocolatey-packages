ebc81cf0815faa1473e7db5b0615012e49941f29
Jeremy Chadwick <jdc@koitsu.org>
2022-12-19 13:42:05 -0800

    Add ucon64

---
+++ ucon64/README.md
@@ -0,0 +1,8 @@
+# ucon64
+
+This is the source repository for the Chocolately package named
+[ucon64](https://community.chocolatey.org/packages/ucon64).
+
+For details about the uCON64 software itself, please refer to
+[the homepage](https://ucon64.sourceforge.io/).
+
---
+++ ucon64/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿# Based on sources:
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/sysinternals/tools/chocolateyInstall.ps1
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
+# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
+
+$ErrorActionPreference = 'Stop'
+
+$packageName = $env:chocolateyPackageName
+$installDir = Split-Path $MyInvocation.MyCommand.Definition
+
+Write-Host "$packageName is going to be installed in: $installDir"
+
+$packageArgs = @{
+  packageName     = $packageName
+  url             = 'https://sourceforge.net/projects/ucon64/files/ucon64/ucon64-2.2.2/ucon64-2.2.2-win32-vc-bin.zip/download'
+  checksum        = '4a4f3e6f46bb61a10db9e1764aa499de23879195aec626aaaed8379aeae3d6f8'
+  checksumType    = 'sha256'
+  unzipLocation   = $installDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
