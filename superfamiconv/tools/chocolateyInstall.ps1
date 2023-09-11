612193ac9b966928ed688313e2c264ec5c4453e8
Jeremy Chadwick <jdc@koitsu.org>
2023-09-10 23:49:48 -0700

    superfamiconv: several improvements
    - Switch to using AU (chocolatey-au) for auto-updating
    - Pre-package the binaries/zips as part of package building
    - Add legal/ files
    - Simplify and clean up install script

--- superfamiconv/tools/chocolateyInstall.ps1
+++ superfamiconv/tools/chocolateyInstall.ps1
@@ -1,22 +1,11 @@
-﻿# Based on sources:
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/sysinternals/tools/chocolateyInstall.ps1
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
+﻿$ErrorActionPreference = 'Stop'
 
-$ErrorActionPreference = 'Stop'
-
-$packageName = $env:chocolateyPackageName
-$installDir = Split-Path $MyInvocation.MyCommand.Definition
-
-Write-Host "$packageName is going to be installed in: $installDir"
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName     = $packageName
-  url64bit        = 'https://github.com/Optiroc/SuperFamiconv/releases/download/v0.9.2/SuperFamiconv-v0.9.2-win_x64.zip'
-  checksum64      = 'd8e3ebcacda5092dd39a54d73ebbccac1e590cabef2cdc4bd0721cf1b58908c7'
-  checksumType64  = 'sha256'
-  unzipLocation   = $installDir
+  packageName     = $env:ChocolateyPackageName
+  fileFullPath64  = "$toolsDir\SuperFamiconv-v0.9.2-win_x64.zip"
+  destination     = $toolsDir
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
+Get-ChocolateyUnzip @packageArgs
---
+++ superfamiconv/update.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module AU
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUrl)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum(64)?\:).*"       = "`${1} $($Latest.Checksum64)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*fileFullPath64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseUrl)`${2}"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $LatestRelease = Get-GitHubRelease -OwnerName Optiroc -RepositoryName SuperFamiconv -Latest
+
+  @{
+    URL64         = $LatestRelease.assets | Where-Object { $_.name -match '-win_x64\.zip$' } | Select-Object -ExpandProperty browser_download_url
+    Version       = $LatestRelease.tag_name.TrimStart("v")
+    ReleaseUrl    = $LatestRelease.html_url
+  }
+}
+
+Update-Package -ChecksumFor none -NoReadme
