75ad91044536ff26802aa1b183e1a73a51bc69eb
Jeremy Chadwick <jdc@koitsu.org>
2023-09-13 23:37:40 -0700

    ucon64: several improvements
    - Switch to using AU (chocolatey-au) for auto-updating
    - Pre-package the binaries/zips as part of package building
    - Add legal/ files
    - Simplify and clean up install script

--- ucon64/tools/chocolateyInstall.ps1
+++ ucon64/tools/chocolateyInstall.ps1
@@ -1,22 +1,11 @@
-﻿# Based on sources:
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/sysinternals/tools/chocolateyInstall.ps1
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/curl/tools/chocolateyInstall.ps1
-# https://github.com/chocolatey-community/chocolatey-packages/blob/master/automatic/wget/tools/chocolateyinstall.ps1
+﻿$ErrorActionPreference = 'Stop'
 
-$ErrorActionPreference = 'Stop'
-
-$packageName = $env:chocolateyPackageName
-$installDir = Split-Path $MyInvocation.MyCommand.Definition
-
-Write-Host "$packageName is going to be installed in: $installDir"
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName     = $packageName
-  url             = 'https://sourceforge.net/projects/ucon64/files/ucon64/ucon64-2.2.2/ucon64-2.2.2-win32-vc-bin.zip/download'
-  checksum        = '4a4f3e6f46bb61a10db9e1764aa499de23879195aec626aaaed8379aeae3d6f8'
-  checksumType    = 'sha256'
-  unzipLocation   = $installDir
+  packageName  = $env:ChocolateyPackageName
+  fileFullPath = "$toolsDir\ucon64-2.2.2-win32-vc-bin.zip"
+  destination  = $toolsDir
 }
 
-Install-ChocolateyZipPackage @packageArgs
-
+Get-ChocolateyUnzip @packageArgs
---
+++ ucon64/update.ps1
@@ -0,0 +1,36 @@
+﻿Import-Module AU
+
+$releases = 'https://sourceforge.net/projects/ucon64/files/ucon64/'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s*32-Bit Software.*)\<.*\>"  = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*fileFullPath\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $content = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $latest = $content.Links.Href `
+            | Where-Object { $_ -Match '/ucon64/ucon64-([0-9\.]+)/$' } `
+            | ForEach-Object { $Matches[1] } `
+            | Sort-Object -Descending { $_ -As [version] } `
+            | Select-Object -First 1
+
+  @{
+    URL32   = 'https://sourceforge.net/projects/ucon64/files/ucon64/ucon64-' + $latest + '/ucon64-' + $latest + '-win32-vc-bin.zip'
+    Version = $latest.TrimStart("v")
+  }
+}
+
+Update-Package -ChecksumFor none -NoReadme
+
