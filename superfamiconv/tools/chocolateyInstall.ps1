435a1e141de834dbca2d379ae8835f4781d476fb
Jeremy Chadwick <jdc@koitsu.org>
2025-01-15 18:03:57 -0800

    superfamiconv: update to v0.11.0

--- superfamiconv/tools/chocolateyInstall.ps1
+++ superfamiconv/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath64 = "$toolsDir\superfamiconv_win64_v0.10.0.zip"
+  fileFullPath64 = "$toolsDir\superfamiconv_win64_v0.11.0.zip"
   destination    = $toolsDir
 }
 
